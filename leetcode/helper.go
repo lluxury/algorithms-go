3a6c361758b69153ab5f234a92be3988114002b5
Chyroc <chen_yunpeng@foxmail.com>
2018-05-18 10:17:15 +0800

    update

--- leetcode/helper.go
+++ leetcode/helper.go
@@ -1,1 +1,15 @@
 package leetcode
+
+func min(s ...int) int {
+	if len(s) == 0 {
+		panic("no value")
+	} else if len(s) == 1 {
+		return s[0]
+	}
+
+	if s[0] > s[1] {
+		return min(append(s[2:], s[1])...)
+	} else {
+		return min(append(s[2:], s[0])...)
+	}
+}
