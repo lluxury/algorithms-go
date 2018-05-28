5a153242700a1c6c37cd27b354c8262eee0a26e1
Chyroc <chen_yunpeng@foxmail.com>
2018-05-29 00:44:50 +0800

    update

---
+++ leetcode/187_repeated_dna_sequences_test.go
@@ -0,0 +1,55 @@
+package leetcode
+
+import (
+	"testing"
+	"sort"
+
+	"github.com/Chyroc/algorithms-go/test"
+)
+
+/*
+> https://leetcode.com/problems/repeated-dna-sequences/description/
+
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+Example:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+
+Output: ["AAAAACCCCC", "CCCCCAAAAA"]
+
+*/
+
+func findRepeatedDnaSequences(s string) []string {
+	var m = make(map[string]int)
+	for i := 0; i+10 <= len(s); i++ {
+		m[s[i:i+10]]++
+	}
+
+	var max int
+	var max_s []string
+	for k, v := range m {
+		if v > max {
+			max = v
+			max_s = []string{k}
+		} else if v == max {
+			max_s = append(max_s, k)
+		}
+	}
+
+	if max > 1 {
+		sort.Strings(max_s)//for test
+		return max_s
+	}
+
+	return nil
+}
+
+func Test_187(t *testing.T) {
+	test.Runs(t, findRepeatedDnaSequences, []*test.Case{
+		{Input: `AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT`, Output: `[AAAAACCCCC,CCCCCAAAAA]`},
+		{Input: `AAAAAAAAAA`, Output: ``},
+	})
+}
---
+++ leetcode/303_range_sum_query_immutable_test.go
@@ -0,0 +1,51 @@
+package leetcode
+
+/*
+
+> https://leetcode.com/problems/range-sum-query-immutable/description/
+
+
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+
+
+* 求一个数组的中，下标i和j之间的数的和，包含i和j
+  * 题目本身不难，直接加起来就行
+  * 但是题目是要求多次运行，能够比较节省时间，所以需要缓存结果或者换一个好一点的办法
+  * i和j之间的数的和 = 前j数之和 - 前i数之和
+*/
+type NumArray struct {
+	nums []int
+}
+
+
+func Constructor_303(nums []int) NumArray {
+	var result = make([]int,len(nums))
+	for k,v :=range nums{
+		if k==0{
+			result[0]=v
+		}else{
+			result[k] = result[k-1]+v
+		}
+	}
+	return NumArray{
+		result,
+	}
+}
+
+
+func (this *NumArray) SumRange(i int, j int) int {
+	if i==0{
+		return this.nums[j]
+	}
+	return this.nums[j]-this.nums[i-1]
+}
