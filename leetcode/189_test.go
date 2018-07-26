b23d3676a83604cfd2d86008cb7f2da497d0ffdb
Chyroc <chyroc@qq.com>
2018-07-26 19:39:35 +0800

    189 rotate_array

---
+++ leetcode/189_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import (
+	"github.com/Chyroc/algorithms-go/test"
+	"testing"
+)
+
+func Test_189(t *testing.T) {
+	test.Runs(t, rotate, []*test.Case{
+		{Input: `[1,2,3,4,5,6,7] \n 3`, Output: `[5,6,7,1,2,3,4]`},
+		{Input: `[-1] \n 2`, Output: `[-1]`},
+	})
+}
