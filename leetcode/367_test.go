94f78408701913b6494bba9067ba1c827160b8e1
Chyroc <chyroc@qq.com>
2018-08-15 20:22:38 +0800

    367_valid_perfect_square.go

---
+++ leetcode/367_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import (
+	"github.com/Chyroc/algorithms-go/test"
+	"testing"
+)
+
+func Test_367(t *testing.T) {
+	test.Runs(t, isPerfectSquare, []*test.Case{
+		{Input: `16`, Output: `true`},
+		{Input: `14`, Output: `false`},
+	})
+}
---
+++ leetcode/367_valid_perfect_square.go
@@ -0,0 +1,67 @@
+package leetcode
+
+import "fmt"
+
+/*
+ * [367] Valid Perfect Square
+ *
+ * https://leetcode.com/problems/valid-perfect-square/description/
+ *
+ * algorithms
+ * Easy (38.81%)
+ * Total Accepted:    80.9K
+ * Total Submissions: 208.4K
+ * Testcase Example:  '16'
+ *
+ * Given a positive integer num, write a function which returns True if num is
+ * a perfect square else False.
+ *
+ *
+ * Note: Do not use any built-in library function such as sqrt.
+ *
+ *
+ * Example 1:
+ *
+ * Input: 16
+ * Returns: True
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: 14
+ * Returns: False
+ *
+ *
+ *
+ * Credits:Special thanks to @elmirap for adding this problem and creating all
+ * test cases.
+ */
+
+ /*
+
+ 判断一个数是不是一个整数的平方
+
+ */
+
+func isPerfectSquare(num int) bool {
+	var x float64 = 1
+	var d int
+	for {
+		x = float64(x+float64(num)/x) / 2
+		fmt.Printf("x is %v\n", x)
+		diff := int(x)*int(x) - num
+		fmt.Printf("diff is %v\n", diff)
+		if diff < 0 {
+			diff = -diff
+		}
+
+		if diff == 0 {
+			return true
+		}
+		if diff == d {
+			return false
+		}
+		d = diff
+	}
+}
