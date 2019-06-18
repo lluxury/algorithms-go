172659271ee59c7410f247fe9ec6c3f825600fb9
yann <tmp4321@qq.com>
2019-06-18 15:00:20 +0800

    50_pow(x,_n)
    Signed-off-by: yann <tmp4321@qq.com>

---
+++ leetcode/50_test.go
@@ -0,0 +1,18 @@
+package leetcode
+
+import (
+    "testing"
+    "github.com/Chyroc/algorithms-go/test"
+)
+
+func Test_50(t *testing.T) {
+    test.Runs(t, myPow, []*test.Case{
+        {Input: `2.00000 \n 10`, Output: `1024.00000`},
+        {Input: `2.10000 \n 3`, Output: `9.26100`},
+        {Input: `2.00000 \n -2`, Output: `0.25000`},
+        })
+}
+
+
+// 第二个测试有一点误差,应该限定输出位数么?
+// 或者是版本的区别?
