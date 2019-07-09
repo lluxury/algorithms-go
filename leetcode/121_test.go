e80966d5c4d7ba91baf52d750b160cb6a219e20d
yann <tmp4321@qq.com>
2019-07-09 11:50:59 +0800

    121_best_time_to_buy_and_sell_stoc

---
+++ leetcode/121_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import (
+	"github.com/Chyroc/algorithms-go/test"
+	"testing"
+)
+
+func Test_121(t *testing.T) {
+	test.Runs(t, maxProfit, []*test.Case{
+        {Input: `[7,1,5,3,6,4]`, Output: `5`},
+		{Input: `[7,6,4,3,1]`, Output: `0`},
+	})
+}
